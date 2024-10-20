c95ce3eab93950f2b8633d043eecf28c1ac54cc0
Karen Etheridge <ether@cpan.org>
2024-10-20 11:30:09 -0700

    increment $VERSION after 2.000011 release

--- lib/Dist/Zilla/Plugin/MinimumVersionTests.pm
+++ lib/Dist/Zilla/Plugin/MinimumVersionTests.pm
@@ -5,7 +5,7 @@ use warnings;
 package Dist::Zilla::Plugin::MinimumVersionTests;
 # ABSTRACT: Release tests for minimum required versions
 
-our $VERSION = '2.000011';
+our $VERSION = '2.000012';
 
 use Moose;
 extends 'Dist::Zilla::Plugin::Test::MinimumVersion';
--- lib/Dist/Zilla/Plugin/Test/MinimumVersion.pm
+++ lib/Dist/Zilla/Plugin/Test/MinimumVersion.pm
@@ -5,7 +5,7 @@ use warnings;
 package Dist::Zilla::Plugin::Test::MinimumVersion;
 # ABSTRACT: Author tests for minimum required versions
 
-our $VERSION = '2.000011';
+our $VERSION = '2.000012';
 
 use Moose;
 with
